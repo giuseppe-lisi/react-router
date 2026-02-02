692f6871b655b743d3a14fe8e45ba448c8fe493b
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-02 18:00:23 +0100

    la navbar è ora presente in tutte le pagine del sito

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,10 +3,17 @@ import { NavLink } from "react-router-dom";
 function Products() {
     return (
         <>
+            <div className="navbar">
+                <h1>Store</h1>
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/Products">Prodotti</NavLink>
+                    <NavLink to="/AboutUs">Info</NavLink>
+                </nav>
+            </div>
             <h2>Questa è la pagina dei prodotti</h2>
         </>
     );
 }
-
 
 export default Products;
