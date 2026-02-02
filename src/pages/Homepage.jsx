ce1839b9e6c7bdc61e8a2208e20b304b6b9630e4
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-02-02 17:59:55 +0100

    creata navbar nella homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 function Homepage() {
     return (
         <>
+            <div className="navbar">
+                <h1>Store</h1>
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/Products">Prodotti</NavLink>
+                    <NavLink to="/AboutUs">Info</NavLink>
+                </nav>
+            </div>
             <h2>Questa è la Homepage</h2>
         </>
     );
